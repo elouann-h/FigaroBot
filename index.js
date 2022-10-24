2d553d721d61b153f41d2ec5ade7cbaddb56b5e1
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-10-24 09:57:57 +0200

    Changed something

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const { REST, Routes } = require('discord.js');
+const { REST, Routes } = require("discord.js");
 
 const Susbot = new Discord.Client({ intents: 32771 });
 
@@ -12,25 +12,25 @@ const commands = [
     {
         name: "roue-des-problemes",
         description: "Créer une roue avec des défis, des questions et des dossiers...",
-    }
+    },
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 if (process.env.REGISTER === "1") {
     (async () => {
         try {
-            console.log('Started refreshing application (/) commands.');
+            console.log("Started refreshing application (/) commands.");
 
             await rest.put(Routes.applicationCommands("1033846760210169967"), { body: commands });
 
-            console.log('Successfully reloaded application (/) commands.');
+            console.log("Successfully reloaded application (/) commands.");
         } catch (error) {
             console.error(error);
         }
     })();
 }
 
-console.log("test")
+console.log("test");
 
 Susbot.login(process.env.TOKEN);
